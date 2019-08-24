4b7b27148c086f5b28b0749c24cf43e4b229e567
Juan Pablo Yamamoto <jpyamamoto9@gmail.com>
2019-08-24 00:49:24 -0500

    Errata

---
+++ Extra1/Erratas.hs
@@ -0,0 +1,73 @@
+-- | Estructuras discretas 2020-1
+-- | Ejercicio Erratas
+-- | Profesora: M. en C. Pilar Selene Linares Arévalo
+-- | Laboratorio: Adrián Felipe Vélez Rivera
+-- | Integrantes:
+-- | Integrante 1: Juan Pablo Yamamoto Zazueta 420002457 jpyamamoto@ciencias.unam.mx
+-- | Integrante 2: Valeria Reyes Tapia 317350113 valeria.tapia@ciencias.unam.mx
+
+module Erratas where
+
+data L = A | J | Q | K deriving(Show, Eq)
+
+data N = N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9 | N10 deriving(Show, Eq)
+
+data F = Trebol | Corazon | Diamante | Espada deriving(Show, Eq)
+
+data S = CartaT1 F N | CartaT2 F L | Comodin deriving(Show,Eq)
+
+type ManoDos = (S,S)
+
+-- | hazManoDos. Hace una dupla de dos manos ingresando los valores de las cartas.
+--
+-- --> hazManoDos (CartaT1 Trebol N2) (CartaT2 Corazon A) = (CartaT1 Trebol N2,CartaT2 Corazon A)
+hazManoDos :: S -> S -> ManoDos
+hazManoDos c1 c2 = (c1,c2)
+
+type ManoT = (S,S,S)
+
+-- | obtenCarta1. Regresa la primera carta de una mano de tres.
+obtenCarta1 :: ManoT -> S
+obtenCarta1 (c1,_,_) = c1
+
+-- | obtenCarta2. Regresa la segunda carta de una mano de tres.
+obtenCarta2 :: ManoT -> S
+obtenCarta2 (_,c2,_) = c2
+
+-- | obtenCarta3. Regresa la tercera carta de una mano de tres.
+obtenCarta3 :: ManoT -> S
+obtenCarta3 (_,_,c3) = c3
+
+-- | manoTresEqMano. Compara si dos manos de tres cartas son iguales.
+manoTresEqMano :: ManoT -> ManoT -> Bool
+manoTresEqMano (c11,c12,c13) (c21,c22,c23) = (c11 == c21) && (c12 == c22) && (c13 == c23)
+
+data R2 = Punto Double Double
+
+instance Show R2 where
+  show (Punto x1 x2) = "("++show x1++","++show x2++")"
+
+-- | v1. Regresa la coordenada x de un punto.
+v1 :: R2 -> Double
+v1 (Punto x y) = x
+
+-- | v2. Regresa la coordenada y de un punto.
+v2 :: R2 -> Double
+v2 (Punto x y ) = y
+
+-- | v. Regresa la diferencia entre las coordenadas de dos puntos.
+v :: R2 -> R2 -> R2
+v p1 p2 = let x = (v1 p2)-(v1 p1) in Punto x y where
+  y = (v2 p2)-(v2 p1)
+
+data Recta = Igual Double Double Double Double
+
+instance Show Recta where
+  show (Igual x v1 y v2) = "(x-"++show x++"/"++show v1++") = (y-"++show y++"/"++show v2++")"
+
+-- | recta. Regresa la ecuación de una recta que se crea a partir de dos puntos.
+recta :: R2 -> R2 -> Recta
+recta p1 p2 = let x1 = v1 p1 in let y1 = v2 p2 in Igual x1 x21 y1 y21 where
+  vd = v p1 p2
+  x21 = v1 vd
+  y21 = v2 vd
