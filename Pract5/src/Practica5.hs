30dd83ace9749b1b0c78fb9bb044de45016075cc
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2019-11-17 23:13:49 -0600

    Trayectorias

--- Pract5/src/Practica5.hs
+++ Pract5/src/Practica5.hs
@@ -20,6 +20,9 @@ exgrado x (_, ys) = exgradoAux x ys
   where exgradoAux x []          = 0
         exgradoAux x ((a, _):ys) = if x == a then 1 + exgradoAux x ys else exgradoAux x ys
 
+trayectorias :: Eq a => a -> Graph a -> [[(a, a)]]
+trayectorias x (_, ys) = construyeCamino x [] ys
+
 esReflexiva :: Eq a => Graph a -> Bool
 esReflexiva ([], _)    = True
 esReflexiva (x:xs, ys) = if (elem (x, x) ys) then esReflexiva (xs, ys) else False
@@ -46,7 +49,6 @@ composicion (x, xs) (y, ys) = (x ++ [z | z <- y, not (elem z x)], [(a, d) | (a, 
 cerrTransitiva :: Eq a => Graph a -> Graph a
 cerrTransitiva (xs, ys) = warshall (xs, ys) (length xs)
 
-
 -- **************************
 -- *                        *
 -- *   Métodos Auxiliares   *
@@ -54,9 +56,16 @@ cerrTransitiva (xs, ys) = warshall (xs, ys) (length xs)
 -- **************************
 
 warshall :: Eq a => Graph a -> Int -> Graph a
-warshall graph 0    = graph
-warshall (xs, ys) n =
+warshall graph 0     = graph
+warshall (xs, ys) n  =
   let preWarshall    = snd (warshall (xs, ys) (n-1))
       transitivo a b = elem (a, b) [(x, z) | x <- xs, y <- xs, z <- xs, elem (x, y) preWarshall && elem (y, z) preWarshall]
   in  (xs, [(a, b) | a <- xs, b <- xs, elem (a, b) preWarshall || transitivo a b])
+
+construyeCamino :: Eq a => a -> [(a, a)] -> [(a, a)] -> [[(a, a)]]
+construyeCamino x cam rel =
+  let caminos = [cam ++ [next] | next <- sucesores x rel, not (elem next cam)]
+      visitado f = sum [1 | (a, _) <- cam, f /= a]
+      sucesores m ns = [(a, b) | (a, b) <- ns, a == m, a /= b, (visitado a) <= 1]
+  in caminos ++ concat [construyeCamino (snd (last camino)) camino rel | camino <- caminos]
 
